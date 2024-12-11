f47ba016d4c414588866feb84124b40f5e9aaea2
JosefinBack <josefin.m.mansson@gmail.com>
2024-12-11 17:02:14 +0100

    Börjat skapa sidan "sum" och fixat själva input-och knapparna överst på sidan

---
+++ webbsidor/index.js
@@ -0,0 +1,31 @@
+
+const contNumber = document.querySelector("containerNumbers");
+
+
+let randomNumbers = [];
+
+function createNumbers (number) {
+    for (let i = 0; i < number; i++) {
+        randomNumbers.push(Math.floor(Math.random() * 101));
+    }
+    return randomNumbers; 
+}
+
+createNumbers(10);
+
+function createDivs () {
+
+    for (let i = 0; i < randomNumbers.length; i++) {
+        let divNumber = document.createElement("div");
+        divNumber.classList.add("numberInDiv");
+        divNumber.textContent = randomNumbers[i];
+        contNumber.appendChild(divNumber);
+    }
+    
+}
+
+createDivs();
+
+
+
+//när man klickar på knappen "create" så ska man kunda ändra hur många kolumner som grid ska ha.
