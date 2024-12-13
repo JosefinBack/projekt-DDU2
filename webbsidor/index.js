e30dfbbd396981acc2ff17892086ac16d0f6c771
JosefinBack <josefin.m.mansson@gmail.com>
2024-12-13 13:08:10 +0100

    Summeringen av nummer under webbsidan SUM fungerar

--- webbsidor/index.js
+++ webbsidor/index.js
@@ -1,6 +1,7 @@
 const contNumber = document.querySelector("#numbers");
 const buttonCreate = document.getElementById("create");
 let inputNumber = document.getElementById("number");
+let inputSumofNumbers = document.getElementById("number1");
 
 let randomNumbers = [];
 
@@ -11,13 +12,6 @@ function createNumbers(number) {
     }
     return randomNumbers;
 }
-
-buttonCreate.addEventListener("click", function () {
-    let number = Number(inputNumber.value); // Hämta värdet från input och konvertera till ett nummer
-    createNumbers(number); // Skapa de slumpmässiga numren
-    createDivs(); // Skapa divar för varje tal
-});
-
 
 function createDivs() {
     contNumber.innerHTML = ""; // Rensa innehållet innan nya divar läggs till
@@ -30,4 +24,26 @@ function createDivs() {
     }
 }
 
+function sumOfNumbers (array) {
+    resultOfNumbers = 0; 
 
+    for(let number of array) {
+        resultOfNumbers = resultOfNumbers + number; 
+    }
+    return resultOfNumbers;
+}
+sumOfNumbers(randomNumbers);
+
+
+/*-----------funktioner slut -------------*/
+
+buttonCreate.addEventListener("click", function () {
+    let number = Number(inputNumber.value); // Hämta värdet från input och konvertera till ett nummer
+    createNumbers(number); // Skapa de slumpmässiga numren
+    createDivs(); // Skapa divar för varje tal
+
+    inputSumofNumbers.textContent = sumOfNumbers(randomNumbers);
+});
+
+
+//när man klickar på divarna då ka man kunna lägga ihop summan i number3
