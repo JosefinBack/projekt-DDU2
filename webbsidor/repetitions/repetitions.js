90c88faf5b6e6ecaa8f6c551b131995f6bb61ee8
JosefinBack <josefin.m.mansson@gmail.com>
2025-01-02 15:58:40 +0100

    Sista delen av repetitions är nu klar

--- webbsidor/repetitions/repetitions.js
+++ webbsidor/repetitions/repetitions.js
@@ -1,9 +1,42 @@
 const numbersMissing = document.getElementById("missingNumbers");
 const repeatedNumbers = document.getElementById("repeatedNumbers");
 
+
+function mostRepeatedNumbers() {
+    const allDivs = document.querySelectorAll(".numberInDiv")
+    let mostRepeatedNumbers = []; 
+    let highestCount = 0; 
+    for(let i = 0; i < randomNumbers.length; i++) {
+        let counter = 0;
+        for(let j = 0; j <randomNumbers.length; j++) {
+            if (randomNumbers[i] === randomNumbers[j]) {
+                counter++; 
+            }
+        }
+        if (counter > highestCount) {
+            highestCount = counter; 
+            mostRepeatedNumbers = [randomNumbers[i]]; 
+        }
+        else if (counter == highestCount) {
+            if (!mostRepeatedNumbers.includes(randomNumbers[i])) {
+                mostRepeatedNumbers.push(randomNumbers[i]); 
+            }
+        }
+    }
+    repeatedNumbers.textContent = `${mostRepeatedNumbers.join(", ")} (Repeated ${highestCount} times)`;
+
+    for (let x = 0; x < numberInDivs.length; x++) {
+        if(mostRepeatedNumbers.includes(Number(numberInDivs[x].textContent))) {
+            numberInDivs[x].classList.add("repeat"); 
+        }
+    }
+    
+}
+
+
+
 function theMissingNumbers() { 
-//de saknade numren 
-let missingNumbers = []; 
+    let missingNumbers = []; 
 
     for (let i = 0; i <= 100; i++) {
         if(!randomNumbers.includes(i)) {//om randomNumber INTE innehåller i
@@ -15,16 +48,14 @@ let missingNumbers = [];
 
 
 buttonCreate.addEventListener("click", function () {
+    mostRepeatedNumbers();
     theMissingNumbers();
-  });
+});
 
 
 
 
-  
+
 //functionsanrop
 theMissingNumbers(); 
-
-
-// jag behöver hitta vilka/ vilket nummer som repeteras flest gånger 
-//alla nummer mellan 0 och 100 som INTE finns representerade i divarna ska stå i boxens
+mostRepeatedNumbers();
